0350eb0e3d7a16cbc0ba2c06098658ca68b8a656
Jason Pien <jason_pien@brown.edu>
2025-01-23 20:00:07 -0500

    revert back to prod non-testing

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ def summarize_and_notify():
    #article_links = get_article_links('dan@tldrnewsletter.com')
     # Alternatively use another email, as needed
     article_links = get_article_links('pien.jason@gmail.com')
-    article_links.append('https://www.forbes.com/sites/stephenpastis/2025/01/21/fintech-payments-startup-highnote-hits-750-million-plus-valuation/')
 
     print(f"Article Links: {article_links}")
     articles_content = scrape_articles(article_links)
